cfae24288dace75f168bc72dfabd663f846464b7
nikolo <n.sugrobov@mail.ru>
2026-05-01 22:13:11 +0300

    perf: enable gzip compression on server responses

--- server/server.js
+++ server/server.js
@@ -12,8 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const compression = require('compression');
+
 app.use(cors());
 app.use(express.json());
+
+app.use(compression());
 
 // Пул с промисами (для всех запросов)
 const db = mysql.createPool({
