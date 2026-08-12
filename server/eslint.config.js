f80cea0fd7db126679ab2677b71685d4acf59a2b
nikolo <n.sugrobov@mail.ru>
2026-08-12 12:53:00 +0300

    fix: extend ESLint config to .cjs/.mjs, align scripts with renamed files

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -4,7 +4,7 @@ import globals from 'globals';
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ['**/*.{js,cjs,mjs}'],
     languageOptions: {
       ecmaVersion: 'latest',
       globals: {
