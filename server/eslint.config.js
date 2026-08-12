1fa27909257f39eab5e2742ba41f449e2a28d50e
nikolo <n.sugrobov@mail.ru>
2026-08-12 09:48:17 +0300

    ci: enable server-side linting in GitHub Actions

---
+++ server/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+        ...globals.jest,   // тесты сервера на Jest
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+    },
+  },
+];
